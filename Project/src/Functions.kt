398a64d1bd6fc092de1e12ae40ef49c901f35d52
ajdinhercinovic <ajdin.hercinovic@fet.ba>
2025-11-01 21:24:54 +0100

    Add files via upload

--- Project/src/Functions.kt
+++ Project/src/Functions.kt
@@ -16,3 +16,59 @@ fun filterRating(lista : List<App>, rating : Double) : List<App>{
 fun filterRatingLambda(lista : List<App>, rating : Double) : List<App> {
     return lista.filter { it.avgRating > rating }
 }
+
+fun categoryCount(apps : List<App>) : Map<String, Int> {
+    val map = mutableMapOf<String, Int>()
+    for(app in apps){
+        if(map.containsKey(app.category)){
+            map[app.category] = map[app.category]!!.plus(1)
+        } else{
+            map[app.category] = 1
+        }
+    }
+    return map.toMap()
+}
+
+fun categoryCountLambda(lista : List<App>) : Map<String, Int> {
+    val map = lista.groupingBy { it.category }.eachCount()
+    return map
+}
+
+fun sortDwDesc(lista : List<App>) : List<App> {
+    val sortirana = lista.toMutableList()
+        for(i in 0 until sortirana.size){
+            var maxIndex = i
+            for(j in i + 1 until sortirana.size){
+                if(sortirana[j].downloads > sortirana[maxIndex].downloads){
+                    maxIndex = j
+                }
+            }
+            if(maxIndex != i){
+                val temp = sortirana[i]
+                sortirana[i] = sortirana[maxIndex]
+                sortirana[maxIndex] = temp
+            }
+        }
+    return sortirana.toList()
+}
+
+fun sortDwDescLambda(lista : List<App>) : List<App> {
+    return lista.sortedByDescending { it.downloads }
+}
+
+fun avgSizePerCategory(lista: List<App>) : Map<String, Double> {
+    val map = lista.groupingBy { it.category }.fold(0 to 0) { avg, app ->
+        val (total, count) = avg
+        (total + app.sizeMB) to (count + 1)
+    }.mapValues { (_,pair) -> pair.first.toDouble() / pair.second }
+    return map
+}
+
+fun searchByName(lista: List<App>, name : String){
+    val found = lista.find { it.appName == name }
+    if(found == null){
+        println("Aplikacija sa imenom:$name nije pronađena!")
+    } else {
+        println("${found.appName} (${found.category}) Downloads:${found.downloads} Rating:${found.avgRating}/5, ${found.sizeMB}MB")
+    }
+}
--- Project/src/Main.kt
+++ Project/src/Main.kt
@@ -5,19 +5,32 @@ import functions.*
 
 fun main() {
     val apps = listOf(
-        App("Instagram", "Social", 150000000, 4.3, 86),
-        App("Facebook", "Social", 120000000, 4.2, 125),
-        App("Youtube", "Entertainment", 180000000, 4.7, 177),
-        App("Olx.ba", "Shopping", 1700000, 4.0, 205),
-        App("Spotify", "Music", 80000000, 4.8, 350),
-        App("Weather&Radar", "Weather", 1500000, 4.7, 56),
-        App("Glovo", "Food", 2000000, 4.6, 82),
-        App("Klix.ba", "News", 1000000, 4.3, 86),
-        App("Viber", "Messaging", 230000000, 4.2, 115),
-        App("Fashion&Friends", "Shopping", 150000000, 4.3, 86)
+        App("Instagram", "Social", 1, 4.3, 86),
+        App("Facebook", "Social", 2, 4.2, 125),
+        App("Youtube", "Entertainment", 3, 4.7, 177),
+        App("Olx.ba", "Shopping", 4, 4.0, 205),
+        App("Spotify", "Music", 5, 4.8, 350),
+        App("Weather&Radar", "Weather", 6, 4.7, 56),
+        App("Glovo", "Food", 7, 4.6, 82),
+        App("Klix.ba", "News", 8, 4.3, 86),
+        App("Viber", "Messaging", 9, 4.2, 115),
+        App("Fashion&Friends", "Shopping", 10, 4.3, 86)
     )
-    val filtrirana = filterRatingLambda(apps, 4.5)
+    /*val filtrirana = filterRatingLambda(apps, 4.5)
     for(app in filtrirana){
         println("${app.appName} Category:${app.category} Downloads:${app.downloads} Rating:${app.avgRating} size:${app.sizeMB}MB")
-    }
+    }*/
+    /*val map = categoryCountLambda(apps)
+    for((category, count) in map){
+        println("$category -> $count")
+    }*/
+    /*val sortirana = sortDwDescLambda(apps)
+    for(app in sortirana){
+        println("${app.appName} Category:${app.category} Downloads:${app.downloads} Rating:${app.avgRating} size:${app.sizeMB}MB")
+    }*/
+    /*val katPoVel = avgSizePerCategory(apps)
+    for((category, avgSize) in katPoVel) {
+        println("$category -> $avgSize")
+    }*/
+    searchByName(apps, "Facebook")
 }
